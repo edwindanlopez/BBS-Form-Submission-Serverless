108d38e3c5b692cccdb188ee90c5ee0393dab926
Daniel Lopez <daniel@bldrscove.com>
2021-09-25 10:52:46 -0400

    Specified console logs for better understanding.

--- api/sendgrid.js
+++ api/sendgrid.js
@@ -1,10 +1,9 @@
 import sendgrid from "@sendgrid/mail";
-
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
 async function sendEmail(req, res) {
   try {
-    console.log("REQ.BODY", req.body);
+    console.log("Logging sendEmal req.body: ", req.body);
     await sendgrid.send({
       to: "daniel@bldrscove.com", // Change to your recipient
       from: "edwin@buildbeautifulspaces.com", // Change to your verified sender
@@ -13,11 +12,13 @@ async function sendEmail(req, res) {
       html: `<div><strong>and easy to do anywhere</strong>, even with Node.js</div>`,
     });
   } catch (error) {
-    console.log(error);
-    return res.status(error.statusCode || 500).json({ error: error.message });
+    console.log("Error on sendEmail Function: ", error);
+    return res
+      .status(error.statusCode || 500)
+      .json({ sendGridError: error.message });
   }
 
-  return res.status(200).json({ Response: req.body });
+  return res.status(200).json({ sendGridResponse: req.body });
 }
 
 const allowCors = (fn) => async (req, res) => {
