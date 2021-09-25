a5fa2d8ee425486999ee3f289f02c516da264ee8
Daniel Lopez <daniel@bldrscove.com>
2021-09-25 03:51:39 -0400

    Added OPTIONS to the set header

--- api/sendgrid.js
+++ api/sendgrid.js
@@ -1,5 +1,4 @@
 import sendgrid from "@sendgrid/mail";
-import micro from "micro-cors";
 
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
@@ -21,6 +20,24 @@ async function sendEmail(req, res) {
   return res.status(200).json({ Response: req.body });
 }
 
-const cors = micro();
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
 
-module.exports = cors(sendEmail);
+module.exports = allowCors(sendEmail);
