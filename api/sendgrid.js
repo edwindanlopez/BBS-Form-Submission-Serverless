3b3fdad62ffd949468dc6d26bd59b146270cb63a
Daniel Lopez <daniel@bldrscove.com>
2021-09-25 03:30:57 -0400

    Trying micro-cors as CORS issue solution

--- api/sendgrid.js
+++ api/sendgrid.js
@@ -1,7 +1,9 @@
 import sendgrid from "@sendgrid/mail";
+import micro from "micro-cors";
+
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
-function sendEmail(req, res) {
+async function sendEmail(req, res) {
   try {
     console.log("REQ.BODY", req.body);
     await sendgrid.send({
@@ -19,24 +21,6 @@ function sendEmail(req, res) {
   return res.status(200).json({ Response: req.body });
 }
 
-const allowCors = (fn) => async (req, res) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  // another common pattern
-  res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
-  return await fn(req, res);
-};
+const cors = micro();
 
-module.exports = allowCors(sendEmail);
+module.exports = cors(sendEmail);
