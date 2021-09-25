dd59f6d307d40f8e4476a0e41898658e37c7faed
Daniel Lopez <daniel@bldrscove.com>
2021-09-25 03:01:55 -0400

    Added CORS policy headers

--- api/sendgrid.js
+++ api/sendgrid.js
@@ -16,7 +16,24 @@ async function sendEmail(req, res) {
     return res.status(error.statusCode || 500).json({ error: error.message });
   }
 
-  return res.status(200).json({ Response: "" });
+  return res.status(200).json({ Response: req.body });
 }
 
-export default sendEmail;
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader("Access-Control-Allow-Methods", "POST");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
+module.exports = allowCors(sendEmail);
