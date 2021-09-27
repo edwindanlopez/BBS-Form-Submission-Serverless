335ae66cadab756399533633d7fd07825525dbed
Daniel Lopez <daniel@bldrscove.com>
2021-09-27 00:53:34 -0400

    Changed order

--- api/sendgrid.js
+++ api/sendgrid.js
@@ -2,24 +2,6 @@ const sendgrid = require("@sendgrid/mail");
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
 module.exports = async function (req, res) {
-  // set permission headers for client
-  res.setHeader("Accept-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
   if (req.method === "POST") {
     const msg = {
       to: "daniel@bldrscove.com", // Change to your recipient
@@ -33,16 +15,34 @@ module.exports = async function (req, res) {
       .send(msg)
       .then(() => {
         res.status(200).json({
+          location: "From within the sendgrid funcion",
           success: true,
           message: "Email sent",
         });
       })
       .catch((error) => {
         console.log("Error on sendEmail Function: ", error);
+        console.log("Error body: ", error.response.body);
         res.status(500).json({
+          location: "From within the sendgrid funcion",
           success: false,
           message: `Server error: ${error}`,
         });
       });
+  }
+  // set permission headers for client
+  res.setHeader("Accept-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader("Access-Control-Allow-Methods", "OPTIONS,POST");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
   }
 };
