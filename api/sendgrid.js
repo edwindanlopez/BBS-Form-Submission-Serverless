0b25f1e545079940951fcc7f19bba789ac7991f1
Daniel Lopez <daniel@bldrscove.com>
2021-09-28 01:00:39 -0400

    Added html formatting and testing image src.

--- api/sendgrid.js
+++ api/sendgrid.js
@@ -4,17 +4,29 @@ sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 const fetchSendGrid = (req, res) => {
   sendgrid
     .send({
-      to: req.body.email, // Change to your recipient
-      from: process.env.SENDGRID_VERIFIED_SENDER, // verified sender
+      to: process.env.SENDGRID_VERIFIED_SENDER, // verified sender
+      from: req.body.email, // submission sender
       subject: req.body.subject,
       message: req.body.message,
       html: `<div>
-        <h1>You're being contacted by: ${req.body.firstName} ${req.body.lastName}</h1>
-        <h5>Their prefered method of contact is: ${req.body["method-of-contact"]} </h5>
-        <h5>You can reach them at ${req.body.phone}, </h5>
-        <h5>Or through their email ${req.body.email}</h5>
+        <h1>You're being contacted by: ${req.body.firstName} ${
+        req.body.lastName
+      }</h1>
+        -------------------------------------------------
+        <h3>Their prefered method of contact is: ${
+          req.body["method-of-contact"]
+        } </h3>
+        -------------------------------------------------
+        <h3>You can reach them at ${req.body.phone}, </h3>
+        <h3>Or through their email ${req.body.email}</h3>
+        -------------------------------------------------
         <p>They've reached out regarding:</p>
         <p>${req.body.message}</p>
+        -------------------------------------------------
+        ${req.body.file && <image src={req.body.file} alt='image-attachment' />}
+        -------------------------------------------------
+        <br/>
+        <p>They live in ${req.body.city}</p>
       </div>`,
     })
     .then(() => {
@@ -44,10 +56,7 @@ const allowCors = (fn) => async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   // another common pattern
   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
+  res.setHeader("Access-Control-Allow-Methods", "OPTIONS,POST");
   res.setHeader(
     "Access-Control-Allow-Headers",
     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
