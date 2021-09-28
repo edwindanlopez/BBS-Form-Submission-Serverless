2243814c35420b52dedaef5973b347395fa11dde
Daniel Lopez <daniel@bldrscove.com>
2021-09-27 21:47:34 -0400

    Moved OPTIONS req handler to the top.

--- api/sendgrid.js
+++ api/sendgrid.js
@@ -2,9 +2,16 @@ const sendgrid = require("@sendgrid/mail");
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
 module.exports = async function (req, res) {
-  // call sendgrid first, since this request doesn't allow any CORS
-  if (req.method === "POST") {
-    const msg = {
+  // handle pre-flight request
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+
+  console.log("req.body.email", req.body.email);
+
+  sendgrid
+    .send({
       to: req.body.email, // Change to your recipient
       from: process.env.SENDGRID_VERIFIED_SENDER, // verified sender
       subject: req.body.subject,
@@ -17,25 +24,34 @@ module.exports = async function (req, res) {
         <p>They've reached out regarding:</p>
         <p>${req.body.message}</p>
       </div>`,
-    };
-
-    sendgrid
-      .send(msg)
-      .then(() => {
-        res.status(200).json({
-          location: "From within the sendgrid funcion",
-          success: true,
-          message: "Email sent",
-        });
-      })
-      .catch((error) => {
-        console.log("Error on sendEmail Function: ", error);
-        console.log("Error body: ", error.response.body);
-        res.status(500).json({
-          location: "From within the sendgrid funcion",
-          success: false,
-          message: `Server error: ${error}`,
-        });
+    })
+    .then(() => {
+      res.status(200).json({
+        location: "From within the sendgrid funcion",
+        success: true,
+        message: "Email sent",
       });
-  }
+    })
+    .catch((error) => {
+      console.log("Error on sendEmail Function: ", error);
+      console.log("Error body: ", error.response.body);
+      res.status(500).json({
+        location: "From within the sendgrid funcion",
+        success: false,
+        message: `Server error: ${error}`,
+      });
+    });
+
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // another common pattern
+  res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
 };
